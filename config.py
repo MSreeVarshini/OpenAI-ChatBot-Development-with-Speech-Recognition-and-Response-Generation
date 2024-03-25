68ad94679dd27e2159d5da18eceff67b55c9942e
M Sree Varshini <123762981+MSreeVarshini@users.noreply.github.com>
2024-03-26 01:05:21 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,2 @@
 # todo: Create your own api key
-#apikey = "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"
-apikey = "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"
-
-#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
-# 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
+apikey = "$$$$$$$$$$$$$$$$$$"
